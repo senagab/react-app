7e1ff63da3e943556481a8cac46efaec6c3415e9
senagab <senagab@gmail.com>
2025-01-27 19:14:57 -0400

    feat: vscode json setting file + prettier setup + custom.d.ts config

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
-import { ThemeProvider } from 'styled-components'
-import Projetos from './containers/Projetos/projetos-index'
-import Sidebar from './containers/Sidebar/sidebar-index'
-import Sobre from './containers/Sobre/sobre-index'
-import EstiloGlobal, { Container } from './styles'
-import temaLight from './themes/light'
-import temaDark from './themes/dark'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [usandoTemaDark, setUsandoTemaDark] = useState(false)
-
-  function trocarTema() {
-    setUsandoTemaDark(!usandoTemaDark)
-  }
-
   return (
-    <ThemeProvider theme={usandoTemaDark ? temaDark : temaLight}>
-      <EstiloGlobal />
-      <Container>
-        <Sidebar trocarTema={trocarTema} />
-        <main>
-          <Sobre />
-          <Projetos />
-        </main>
-      </Container>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
--- src/components/Avatar/avatar-index.tsx
+++
@@ -1,4 +0,0 @@
-import { FotoPerfil } from './avatar-styles'
-const Avatar = () => <FotoPerfil src="https://github.com/AndreDG88.png" />
-
-export default Avatar
