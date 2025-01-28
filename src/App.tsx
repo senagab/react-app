82b06c32130a1a4e9ea03ce21f3914108f04a2af
senagab <senagab@gmail.com>
2025-01-28 19:59:06 -0400

    feat: containers: sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Teste from './teste'
 import EstiloGlobal from './styles'
+import Sidebar from './containers/Sidebar'
 
 function App() {
   return (
     <>
       <EstiloGlobal />
       <Teste />
+      <Sidebar />
     </>
   )
 }
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,7 @@
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/senagab.png" alt="" />
+  </aside>
+)
+
+export default Sidebar
